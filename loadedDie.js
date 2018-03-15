cd7b81fc2ad6a853c85e5fac3f8df4a18bf56fc3
Wendy Braun <wendocalrissian@gmail.com>
2018-03-15 22:53:47 +0000

    Adding loadedDie.js

---
+++ loadedDie.js
@@ -0,0 +1,15 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var roll = -1
+
+  return function() {
+    roll += 1;
+    return list[roll];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
