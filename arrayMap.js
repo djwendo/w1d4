b0936304b9c891bf94786aaddb9957cf6ec0d114
Wendy Braun <wendocalrissian@gmail.com>
2018-03-15 19:16:53 +0000

    Updated arrayMap.js with final code

--- arrayMap.js
+++ arrayMap.js
@@ -1,18 +1,14 @@
-// var input = [ { x: 3, y: 4 }, { x: 12, y: 5 }, { x: 8, y: 15 } ];
-
-
 var input = [
   { x: 3, y: 4 },
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
 
-var result = input.map(input => {
+var result = input.map(obj => {
    var newObject = {};
-   newObject[input.x] = input.x * input.x;
-   console.log(newObject[input.x]);
-   // newObject[obj.y] = obj.y * obj.y;
-   // newObject = Math.sqrt(newObject[obj.x] + newObject[obj.y])
+   newObject[obj.x] = obj.x * obj.x;
+   newObject[obj.y] = obj.y * obj.y;
+   newObject = Math.sqrt(newObject[obj.x] + newObject[obj.y])
    return newObject;
 });
 
